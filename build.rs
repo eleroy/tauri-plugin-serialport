a8cef5bf90bd70fdc112a2279a8be87e3e6c3ebc
Edouard Leroy <edouard.leroy@cea.fr>
2024-02-07 17:04:31 +0100

    - test tauri v2

---
+++ build.rs
@@ -0,0 +1,17 @@
+// Copyright 2019-2023 Tauri Programme within The Commons Conservancy
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-License-Identifier: MIT
+
+const COMMANDS: &[&str] = &["available_ports",
+"cancel_read",
+"close",
+"close_all",
+"force_close",
+"open",
+"read",
+"write",
+"write_binary"];
+
+fn main() {
+    tauri_plugin::Builder::new(COMMANDS).build();
+}
